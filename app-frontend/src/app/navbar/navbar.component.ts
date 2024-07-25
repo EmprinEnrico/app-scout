08ccb97324617cff21bc75c4269986456b8819f8
Enrico Emprin <enrico@Mac-mini.local>
2024-07-25 11:38:17 +0200

    Updated navbar graphics with icons

--- app-frontend/src/app/navbar/navbar.component.ts
+++ app-frontend/src/app/navbar/navbar.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatButtonToggle, MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatIcon, MatButtonToggle, MatButtonToggleModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.less'
 })
 export class NavbarComponent {
+  hideSingleSelectionIndicator = signal(true);
 
 }
